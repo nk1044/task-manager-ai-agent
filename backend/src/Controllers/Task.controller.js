5c303b0fe84903923ca468ff2e4645840c36756e
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-20 13:28:07 +0530

    add CRUD operations for tasks: implement GetAllTasks, UpdateTask, and DeleteTask

--- backend/src/Controllers/Task.controller.js
+++ backend/src/Controllers/Task.controller.js
@@ -39,11 +39,75 @@ const CreateTask = async function (req, res) {
 
 // Get All Tasks -> get
 
+const GetAllTasks = async function (req, res) {
+    try {
+        const tasks = await Task.find();
+        if (tasks) {
+            return res.status(200).json({message:"All Tasks Fetched successfully", data: tasks});
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal server error"});
+    }
+}
+
 // update a Task -> put
 
+const UpdateTask = async function (req, res) {
+    
+    const {id, title, description, status} = req.body;
+
+    try {
+        const TaskExists = await Task.findById(id);
+
+        if (!TaskExists) {
+            return res.status(404).json({message: "Task not found"});
+        }
+
+        const updatedTask = await Task.findByIdAndUpdate(id, {
+            title: title,
+            description: description,
+            status: status
+        }, {new: true});
+
+        if (updatedTask) {
+            return res.status(200).json({message: "Task updated successfully", data: updatedTask});
+        }
+        return res.status(500).json({message: "Error while updating task"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
 // delete a Task -> delete
+
+const DeleteTask = async function (req, res) {
+    const {id} = req.params
+    
+    try {
+
+        const TaskExists = await Task.findById(id);
+        if (!TaskExists) {
+            return res.status(404).json({message: "Task not found"});
+        }
+
+        const deletedTask = await Task.findByIdAndDelete(id);
+        if(deletedTask) {
+            return res.status(200).json({message: "Task deleted successfully"});
+        }
+        return res.status(500).json({message: "Internal server error while deleting task"});
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal server error while deleting task"});
+    }
+}
 
 
 export {
     CreateTask,
+    GetAllTasks,
+    UpdateTask,
+    DeleteTask
 };
--- backend/src/Routes/Task.route.js
+++ backend/src/Routes/Task.route.js
@@ -1,10 +1,13 @@
 import Router from "express";
-import { CreateTask } from "../Controllers/Task.controller.js";
+import { CreateTask, GetAllTasks, UpdateTask, DeleteTask } from "../Controllers/Task.controller.js";
 
 
 const router = Router();
 
 router.route("/tasks").post(CreateTask);
+router.route("/tasks").get(GetAllTasks);
+router.route("/task").put(UpdateTask);
+router.route("/tasks/:id").delete(DeleteTask);
 
 
 export default router;
